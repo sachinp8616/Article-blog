b13fe910ef090476137e53599450dd1099e9c432
sachin <sachinp8616@gmail.com>
2021-03-16 11:33:23 +0530

    second-commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD_wYE2KiXSxQmCBz_82pM57TL-62QyO5s",
-    authDomain: "fir-crud-969ae.firebaseapp.com",
-    databaseURL: "https://fir-crud-969ae-default-rtdb.firebaseio.com",
-    projectId: "fir-crud-969ae",
-    storageBucket: "fir-crud-969ae.appspot.com",
-    messagingSenderId: "259461240864",
-    appId: "1:259461240864:web:62116b89cc633102318445"
-  }
+    apiKey: 'AIzaSyD_wYE2KiXSxQmCBz_82pM57TL-62QyO5s',
+    authDomain: 'fir-crud-969ae.firebaseapp.com',
+    databaseURL: 'https://fir-crud-969ae-default-rtdb.firebaseio.com',
+    projectId: 'fir-crud-969ae',
+    storageBucket: 'fir-crud-969ae.appspot.com',
+    messagingSenderId: '259461240864',
+    appId: '1:259461240864:web:62116b89cc633102318445',
+  },
 };
